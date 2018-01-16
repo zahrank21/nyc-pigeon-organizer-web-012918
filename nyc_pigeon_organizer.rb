b67bdea38913c77397eb87a7c769327163380462
zahrank21 <zahrank@gmail.com>
2018-01-16 16:31:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, trait_hash|
+    trait_hash.each do |trait, names|
+      names.each do |name|
+        pigeon_list[name] = {:color => [],
+          :gender => [],
+          :lives => []
+        }
+      end
+    end
+  end
+  data.each do |category, trait_hash|
+    trait_hash.each do |trait, names|
+      pigeon_list.keys.each do |bird_name|
+        if names.include?(bird_name)
+          pigeon_list[bird_name][category] << trait.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
