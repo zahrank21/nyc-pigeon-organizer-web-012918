d1e6cf482c272622d8ceb29644eddfd0735ca747
zahrank21 <zahrank@gmail.com>
2018-01-16 16:19:25 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |category, trait_hash|
+
+    trait_hash.each do |trait, names|
+
+      names.each do |name|
+
+        pigeon_list[name] = {:color => nil,
+          :gender => nil,
+          :lives => nil
+        }
+      end
+    end
+  end
+  pigeon_list
 end
