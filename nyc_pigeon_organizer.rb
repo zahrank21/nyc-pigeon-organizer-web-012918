4748b03abcb37d20bc0ec715bd465b374b0b4c4e
zahrank21 <zahrank@gmail.com>
2018-01-16 16:28:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,12 +7,22 @@ def nyc_pigeon_organizer(data)
 
       names.each do |name|
 
-        pigeon_list[name] = {:color => nil,
-          :gender => nil,
-          :lives => nil
+        pigeon_list[name] = {:color => [],
+          :gender => [],
+          :lives => []
         }
+
+        
       end
     end
+  end
+  data.each do |category, trait_hash| 
+    trait_hash.each do |trait, names|
+      pigeon_list.keys.each do |bird_name|
+        if names.include?(bird_name)
+          pigeon_list[bird_name][category] << trait
+        end
+      end
   end
   pigeon_list
 end
